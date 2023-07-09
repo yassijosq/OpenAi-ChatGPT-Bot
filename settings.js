6d7a4be76ac1acf7690d81efd06417c904c3a7ad
Yasser <136765083+yassijosq@users.noreply.github.com>
2023-07-09 11:45:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,8 @@ const chalk = require('chalk')
 🤖 𝗕𝗢𝗧 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OFFLINE = false // if you want put bot offline put true
-global.BOT_PRIVATE = true // if you want use bot privet 
-global.AUTO_CHATGPT = false // if you want auto ai bot mod
+global.BOT_PRIVATE = false // if you want use bot privet 
+global.AUTO_CHATGPT = true // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
 global.ONLY_GROUPS = false // Only Groups Use
@@ -16,18 +16,18 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
+global.OWNER_NAME = '𝘛𝘖𝘔𝘙𝘛 𝘖𝘍𝘊' // Owner Name
+global.OWNER_NUMBER = ['212645866783'] //Owner Number
+global.BOT_ADMINS = ['212645866783','212645866783'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
-global.CAPTION = '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ᴅᴀʀᴋ-ᴍᴀᴋᴇʀ-ᴏꜰᴄ ᴀɪ ʙᴏᴛ'
+global.CAPTION = '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ 🇭‌4🇽‌🇽‌ 🇬‌🇵‌🇹‌ 🇧‌🇾‌ 🇹‌🇴‌🇲‌🇷‌🇹‌ ᴀɪ ʙᴏᴛ'
 
 /*
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = ''
+global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = ''
 
 /*
